52748d18c5a43a70e990ba1cdd2a7eae70925b2d
hmducanh <qliphotducanh@gmail.com>
2021-04-29 04:12:44 +0700

    Chua Controller ( van dang co loi )

---
+++ MISA.CukCuk.API/Controllers/CustomerController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.core.Interfaces.Repository;
+using MISA.Core.Entities;
+using MISA.Core.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var customers = _customerService.GetAll();
+            if (customers.Count() > 0)
+            {
+                return Ok(customers);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByid(Guid id)
+        {
+            var customer = _customerService.GetById(id);
+            if (customer == null)
+            {
+                return StatusCode(404, new { error = "Id khong ton tai" });
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+            var res = _customerService.Insert(customer);
+            if (res > 0)
+            {
+                return StatusCode(201, customer);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, Customer customer)
+        {
+            var res = _customerService.Update(customer);
+            if (res > 0)
+            {
+                return StatusCode(200);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Detele(Guid id)
+        {
+            var res = _customerService.Delete(id);
+            if (res > 0)
+            {
+                return StatusCode(200);
+            }
+            return NoContent();
+        }
+    }
+}
